9073976367d9b1866b1cba7133183cd3e9872ef2
doflare123 <salaticvenegret@gmail.com>
2024-03-20 20:54:59 +0200

    change chose current Player and add new logic random players

--- src/main/java/com/example/demo2_1/HelloApplication.java
+++ src/main/java/com/example/demo2_1/HelloApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo2_1;
-import java.util.Random;
+
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
@@ -13,41 +11,33 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import java.util.Random;
 
 public class HelloApplication extends Application {
     protected int count;
     private int Xpos;
     private int Ypos;
-    Image krestic = new Image("C:\\Users\\ABOBUS2006\\IdeaProjects\\demo2.1\\idef\\krestic.png");
-    Image nolic = new Image("C:\\Users\\ABOBUS2006\\IdeaProjects\\demo2.1\\idef\\nolic.png");
-    int currentPlayer;
+    private final Players player1 = new Players(1, "C:\\Users\\ABOBUS2006\\IdeaProjects\\demo2.1\\idef\\krestic.png");
+    private final Players player2 = new Players(2, "C:\\Users\\ABOBUS2006\\IdeaProjects\\demo2.1\\idef\\nolic.png");
     private final int[][] pole = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
     public HelloApplication() {
     }
 
-    public static int random3() {return (new Random()).nextBoolean() ? 1 : 2;}
-
     public void start(Stage primaryStage) {
-        boolean playWithBot = GameModeDialog.show();
+        //boolean playWithBot = GameModeDialog.show();
         Group arena = getGroup();
         Pane root = new Pane();
         GameLogic gameLogic = new GameLogic(pole);
-        count = random3();
+        int currentPlayer = (new Random()).nextBoolean() ? player1.getId() : player2.getId();
+
 
         root.addEventFilter(MouseEvent.MOUSE_CLICKED, (MouseEvent) -> {
             ++count;
-            Xpos = (int)Math.floor(MouseEvent.getX() / 100.0);
-            Ypos = (int)Math.floor(MouseEvent.getY() / 100.0);
+            Xpos = (int) Math.floor(MouseEvent.getX() / 100.0);
+            Ypos = (int) Math.floor(MouseEvent.getY() / 100.0);
             if (pole[Xpos][Ypos] == 0) {
-                ImageView imageView;
-                if (count % 2 == 0) {
-                    imageView = new ImageView(krestic);
-                    currentPlayer = 2;
-                } else {
-                    imageView = new ImageView(nolic);
-                    currentPlayer = 1;
-                }
+                ImageView imageView = new ImageView(currentPlayer == player1.getId() ? player1.getImage() : player2.getImage());
 
                 pole[Xpos][Ypos] = currentPlayer;
                 imageView.setX((25 + 100 * Xpos));
@@ -57,21 +47,20 @@ public class HelloApplication extends Application {
                 root.getChildren().add(imageView);
                 if (gameLogic.checkWin(currentPlayer)) {
                     showGameResult(primaryStage, "Победитель: игрок " + currentPlayer);
-                }
-                else if (gameLogic.allCellsFilled()) {
+                } else if (gameLogic.allCellsFilled()) {
                     showGameResult(primaryStage, "Ничья!");
                 }
             } else {
                 count--;
             }
-
         });
+
         StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(new Node[]{arena, root});
+        stackPane.getChildren().addAll(arena, root);
         Scene scene = new Scene(stackPane, 300.0, 300.0);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
-        primaryStage.setTitle("Line Example");
+        primaryStage.setTitle("Крестики-нолики");
         primaryStage.show();
     }
 
@@ -108,10 +97,12 @@ public class HelloApplication extends Application {
         line3.setStartY(100.0);
         line3.setEndX(300.0);
         line3.setEndY(100.0);
-        return new Group(new Node[]{line, line1, line2, line3});
+        return new Group(line, line1, line2, line3);
     }
 
     public static void main(String[] args) {
-        launch(new String[0]);
+        launch();
     }
+
+
 }
